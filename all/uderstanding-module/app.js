12e9a366f01529ecdfaddc4aa9abee89d8e9b97e
tawhidib <tawhidib@gmail.com>
2022-08-31 18:49:51 +0600

    uderstanding modules basic

---
+++ all/uderstanding-module/app.js
@@ -0,0 +1,7 @@
+const basic = require("./basic");
+
+const { tawhid, salman } = basic.names;
+const sayHi = basic.sayHi;
+
+sayHi(tawhid);
+sayHi(salman);
---
+++ all/uderstanding-module/basic.js
@@ -0,0 +1,9 @@
+const tawhid = "tawhid";
+const salman = "salman";
+
+const sayHi = (name) => {
+  console.log(`Hello ${name}`);
+};
+
+module.exports.names = { tawhid, salman };
+module.exports.sayHi = sayHi;
