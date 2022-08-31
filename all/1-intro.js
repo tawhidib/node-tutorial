0edb7df9563dea930a1c2a580e1b6773e6485faa
tawhidib <tawhidib@gmail.com>
2022-08-31 18:41:32 +0600

    intro and global

---
+++ all/1-intro.js
@@ -0,0 +1,4 @@
+const amount = 12;
+
+if (amount < 10) console.log("amount is small");
+else console.log("amount is big");
---
+++ all/2-global.js
@@ -0,0 +1,13 @@
+// GLOBAL -- NO WINDOW
+
+// __dirname - path to current directory
+// __filename - file name
+// require - function to use modules (CommonJS)
+// module - info about current module(file)
+// process - info about env where the program is begin executed
+
+console.log(__filename);
+// console.log(global);
+// console.log(process);
+
+console.log(module.filename);
